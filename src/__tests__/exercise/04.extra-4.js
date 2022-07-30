f51341ea98a85ff65dd7fd37675768d40947b83b
diomededavid <david@d2creativedesign.com>
2022-07-30 15:11:33 -0700

    uses test data sources and tools

--- src/__tests__/exercise/04.extra-4.js
+++ src/__tests__/exercise/04.extra-4.js
@@ -1,20 +1,21 @@
 // form testing
 // http://localhost:3000/login
+// noinspection JSAnnotator
 
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
-// Allow For overrides
-function buildLoginForm(overrides) {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides,
-  }
-}
+// Uses test Databot
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
+
 test('submitting the form calls onSubmit with username and password', async () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
