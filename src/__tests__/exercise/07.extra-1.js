c68083371948adfcbca773a0844f997f724d35cf
David Diomede <david@d2creativedesign.com>
2022-08-06 16:27:39 -0700

    tests Context and custom render method

--- src/__tests__/exercise/07.extra-1.js
+++ src/__tests__/exercise/07.extra-1.js
@@ -8,12 +8,10 @@ import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-
-  render(
-    <ThemeProvider>
-      <EasyButton>Easy</EasyButton>
-    </ThemeProvider>,
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
   )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
